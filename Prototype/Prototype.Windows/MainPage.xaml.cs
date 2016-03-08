834759c2fa786b3c1a7f7d2764cda4c5727e8b39
Erica Wright <e.a.wright03@gmail.com>
2016-03-07 19:18:59 -0500

    Created dynamic taxa page

--- Prototype/Prototype.Windows/MainPage.xaml.cs
+++ Prototype/Prototype.Windows/MainPage.xaml.cs
@@ -57,31 +57,24 @@ namespace Prototype
         {
             App.f.C = new CharactersBlock();
             List<string> errors = new List<string>();
-            String s = textBox.Text.ToString();
-            String[] array = s.Split(new char[] { ' ' });
-            foreach (object child in BodyPanel.Children)
+            List<TextBox> tbErrors = new List<TextBox>();
+            for (int i = 0; i < TaxaText.Count; i++)
             {
-                TextBox box = (TextBox)child; // we should tyoe check this
-                if (box.Text.Length == 0)
+                TaxaText[i].Background = new SolidColorBrush(Colors.LightGray);
+                if (string.IsNullOrEmpty(TaxaText[i].Text))
                 {
-                    errors.Add("Must enter taxa or adjust the number of fields");
-                    box.Background = new SolidColorBrush(Colors.LightSalmon);
+                    tbErrors.Add(TaxaText[i]);
+                    errors.Add("Empty Taxa value. Must enter Taxa or remove row.");
+                    TaxaText[i].Background = new SolidColorBrush(Colors.LightSalmon);
                 }
-                else
-                {
-                    App.f.C.taxa.Add(box.Text.ToString());
-                }
-
             }
-            if (errors.Count == 0)
+                if (errors.Count == 0)
             {
                 this.Frame.Navigate(typeof(CharactersPage), App.f.C);
             }
             else
             {
             
-            if (errors.Count > 0)
-            {
                 TextBox ErrorText = new TextBox();
                 ErrorText.Name = "errors";
                 ErrorText.Width = 300;
@@ -99,26 +92,90 @@ namespace Prototype
                 sp.HorizontalAlignment = HorizontalAlignment.Center;
                 sp.Children.Add(ErrorText);
                 ScrollError.Content = sp;
-            }
+            
         }
             
         }
 
-        private void btnMakeFields_Click(object sender, RoutedEventArgs e)
+        //private void btnMakeFields_Click(object sender, RoutedEventArgs e)
+        //{
+        //    var fields = Int32.Parse(textBox.Text.ToString()); //need to validate data to prevent exception.
+        //    for (int j = 0; j < BodyPanel.Children.Count(); j++)
+        //    {
+        //        BodyPanel.Children.RemoveAt(j);
+        //        TaxaText.RemoveAt(j);
+        //    }
+        //    for (var i = 0; i < fields; i++)
+        //    {
+        //        TextBox tb = new TextBox() { Text = "animal" + i, Name = "textBox" + i, Margin = new Thickness(0, 10, 0, 0), Width = 150, HorizontalAlignment = HorizontalAlignment.Center };
+        //        TaxaText.Add(tb);
+        //        BodyPanel.Children.Add(tb);
+        //    }
+        //}
+
+
+
+
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
         {
-            var fields = Int32.Parse(textBox.Text.ToString()); //need to validate data to prevent exception.
-            for (int j = 0; j < BodyPanel.Children.Count(); j++)
+            //remove the stackpanel that this button is located in
+            //   sender.
+            var dc = (sender as Button).Parent as StackPanel;
+            for (int i = 0; i < TaxaText.Count; i++)
             {
-                BodyPanel.Children.RemoveAt(j);
-                TaxaText.RemoveAt(j);
+                if (dc.Children.Contains(TaxaText[i]))
+                {
+                    TaxaText.Remove(TaxaText[i]);
+                }
             }
-            for (var i = 0; i < fields; i++)
-            {
-                TextBox tb = new TextBox() { Text = "animal" + i, Name = "textBox" + i, Margin = new Thickness(0, 10, 0, 0), Width = 150, HorizontalAlignment = HorizontalAlignment.Center };
-                TaxaText.Add(tb);
-                BodyPanel.Children.Add(tb);
-            }
+
+           (dc.Parent as StackPanel).Children.Remove(dc);
+
+            TaxaCount.Text = TaxaText.Count.ToString();
+            //remove the textboxes from the list of textboxes a the tpo
         }
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            //add stackpanel with text boxes and remove button
+            TextBox tbTaxa = new TextBox();
+            tbTaxa.AcceptsReturn = true;
+            Thickness m1 = tbTaxa.Margin;
+            m1.Left = 10;
+            tbTaxa.Margin = m1;
+            tbTaxa.Width = 100;
+            tbTaxa.Height = 30;
+            Thickness m3 = tbTaxa.Padding;
+            m3.Left = 200;
+            m3.Bottom = 20;
+            tbTaxa.FontSize = 14;
+            // tbTaxa.Padding = 15;
+            TaxaText.Add(tbTaxa);
+
+            Button btnRemove = new Button();
+            btnRemove.Content = "Remove";
+            btnRemove.IsEnabled = true;
+            btnRemove.HorizontalAlignment = HorizontalAlignment.Right;
+            btnRemove.Click += btnRemove_Click;
+
+            StackPanel s = new StackPanel();
+            s.Orientation = Orientation.Horizontal;
+            s.HorizontalAlignment = HorizontalAlignment.Center;
+            s.Children.Add(tbTaxa);
+            s.Children.Add(btnRemove);
+            // s.Children.Add();
+            //need to add remove button for each row then add button at the bottim
+            BodyPanel.Children.Add(s);
+
+            ScrollTaxa.Content = BodyPanel;
+            TaxaCount.Text = TaxaText.Count.ToString();
+
+        }
+
+
+
+
+
     }
 }
 
